1bb3af35215722b8aef4bf1fa2f3b541b7da4ae6
Derek Johnson <dej3tc@virginia.edu>
2025-03-13 15:33:14 -0400

    Add custom login page

--- src/main/java/com/example/DemoAuthenticator/DemoAuthenticatorApplication.java
+++ src/main/java/com/example/DemoAuthenticator/DemoAuthenticatorApplication.java
@@ -9,14 +9,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,10 +31,14 @@ import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.stereotype.Service;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -128,7 +135,41 @@ public class DemoAuthenticatorApplication {
       }
     }
 
-  }
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+        throws Exception {
+      OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+      http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+          .oidc(Customizer.withDefaults()); // Enable OpenID Connect 1.0
+      http
+          // Redirect to the login page when not authenticated from the
+          // authorization endpoint
+          .exceptionHandling((exceptions) -> exceptions
+              .authenticationEntryPoint(
+                  new LoginUrlAuthenticationEntryPoint("/login")))
+          // Accept access tokens for User Info and/or Client Registration
+          .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+      return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+        throws Exception {
+      http
+          .authorizeHttpRequests((authorize) -> authorize
+              .anyRequest().authenticated())
+          // Form login handles the redirect to the login page from the
+          // authorization server filter chain
+          .formLogin((formLogin) -> formLogin
+              .loginPage("/login")
+              .permitAll());
+      return http.build();
+    }
+
+  } // Close AuthServerConfig
 
   @Service
   public class OidcUserInfoService {
